d453992cfa889b06cf82cd5e2cf835ecf6ce2c4f
John Daly <john.daly2@gmail.com>
2024-01-12 13:57:04 -0800

    Getting changes in place

--- config.mjs
+++ config.mjs
@@ -1,34 +1,54 @@
-import path from "path";
-import { fileURLToPath } from "url";
-import HtmlWebpackPlugin from "html-webpack-plugin";
+import path from 'path';
+import { fileURLToPath } from 'url';
+import HtmlWebpackPlugin from 'html-webpack-plugin';
+import { rspack } from '@rspack/core';
+import TerserPlugin from 'terser-webpack-plugin';
 
 const __dirname = path.dirname(fileURLToPath(import.meta.url));
 const isRunningWebpack = !!process.env.WEBPACK;
 const isRunningRspack = !!process.env.RSPACK;
 if (!isRunningRspack && !isRunningWebpack) {
-  throw new Error("Unknown bundler");
+  throw new Error('Unknown bundler');
 }
 
 /**
  * @type {import('webpack').Configuration | import('@rspack/cli').Configuration}
  */
 const config = {
-  mode: "development",
+  mode: 'development',
   devtool: false,
   entry: {
-    main: "./src/index",
+    main: './src/index',
   },
   plugins: [new HtmlWebpackPlugin()],
   output: {
     clean: true,
     path: isRunningWebpack
-      ? path.resolve(__dirname, "webpack-dist")
-      : path.resolve(__dirname, "rspack-dist"),
-    filename: "[name].js",
+      ? path.resolve(__dirname, 'webpack-dist')
+      : path.resolve(__dirname, 'rspack-dist'),
+    filename: '[name].js',
   },
-  experiments: {
-    css: true,
+  optimization: {
+    minimize: true,
+    minimizer: isRunningRspack
+      ? [
+          new rspack.SwcJsMinimizerRspackPlugin({
+            mangle: true,
+          }),
+        ]
+      : [
+          new TerserPlugin({
+            terserOptions: { mangle: true },
+          }),
+        ],
   },
+  experiments: {},
 };
+
+if (isRunningRspack) {
+  config.experiments.rspackFuture = {
+    newTreeshaking: true,
+  };
+}
 
 export default config;
--- src/index.js
+++ src/index.js
@@ -1,3 +1,6 @@
-import('./render').then(exports => {
-    exports.render()
-})
+export const variableWithLongNameOne = 'one';
+export const variableWithLongNameTwo = 'two';
+export const variableWithLongNameThree = 'three';
+export const variableWithLongNameFour = 'four';
+export const variableWithLongNameFive = 'five';
+export const variableWithLongNameSix = 'six';
